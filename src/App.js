a237fc66092c16d33e24dc7aa2be06289fee224b
Ruslan <kremcheev-ruslan@gmail.com>
2023-03-31 11:44:09 +0300

    client to netlify

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing, Error, Register, ProtectedRoute } from "./pages";
+import { ToastContainer } from "react-toastify";
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  Stats,
+  SharedLayout,
+} from "./pages/dashboard/index";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <ToastContainer position="top-center" />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./features/user/userSlice";
+import jobSlice from "./features/jobs/jobSlice";
+import allJobsSlice from "./features/allJobs/allJobsSlice";
+
+export const store = configureStore({
+  reducer: {
+    user: userSlice,
+    job: jobSlice,
+    allJobs: allJobsSlice,
+  },
+});
